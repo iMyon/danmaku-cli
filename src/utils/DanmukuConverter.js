a47d855072d4e291908adbf989a24cfc78e9d4e2
Myon <myon.cn@gmail.com>
2019-07-17 16:23:13 +0800

    使用canvas精确计算弹幕宽度

--- src/utils/DanmukuConverter.js
+++ src/utils/DanmukuConverter.js
@@ -1,4 +1,8 @@
 const XmlJs = require('xml-js');
+const { createCanvas } = require('canvas');
+const canvas = createCanvas(200, 200);
+const canvasContext = canvas.getContext('2d'); // 用于计算一条弹幕实际占用像素宽度
+
 class DanmukuConverter {
   constructor(config = {}) {
     this.config = {
@@ -6,7 +10,7 @@ class DanmukuConverter {
       PlayResY: 1080, // 分辨率 高
       font: '微软雅黑', // 字体
       bold: true, // 是否加粗
-      font_size: 30, // 字体大小
+      fontSize: 30, // 字体大小
       lineCount: 50, // 弹幕最大行数
       speed: 12, // 滚动弹幕驻留时间（秒），越小越快
       fixedSpeed: 4, // 顶端/底部弹幕驻留时间（秒），越小越快
@@ -15,13 +19,15 @@ class DanmukuConverter {
     Object.assign(this.config, config);
     let alpha16 = this.config.alpha.toString(16);
     this.config.alpha = prefixInteger(alpha16, 2);
-    if (this.config.font_size * this.config.lineCount > this.config.PlayResY) {
-      this.config.lineCount = ~~(this.config.PlayResY / this.config.font_size);
+    if (this.config.fontSize * this.config.lineCount > this.config.PlayResY) {
+      this.config.lineCount = ~~(this.config.PlayResY / this.config.fontSize);
     }
+    canvasContext.font = `${this.config.fontSize}px ${this.config.font}`;
   }
   convert(xmlString) {
-    const xmlJson = JSON.parse(XmlJs.xml2json(xmlString, { compact: true }));
+    const xmlJson = XmlJs.xml2js(xmlString, { compact: true });
     const dElements = xmlJson.i.d;
+
     let danmukuHeader = `\ufeff[Script Info]
 ScriptType: v4.00+
 Collisions: Normal
@@ -31,11 +37,13 @@ PlayResY: ${this.config.PlayResY}
 [V4+ Styles]
 Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding
 Style: Default,微软雅黑,54,&H00FFFFFF,&H00FFFFFF,&H00000000,&H00000000,0,0,0,0,100,100,0.00,0.00,1,1,0,2,20,20,120,0"
-Style: Danmaku,${this.config.font},${this.config.font_size},&H${this.config.alpha}FFFFFF,&H${
+Style: Danmaku,${this.config.font},${this.config.fontSize},&H${this.config.alpha}FFFFFF,&H${
       this.config.alpha
     }FFFFFF,&H${this.config.alpha}000000,&H${this.config.alpha}000000,${~~this.config
       .bold},0,0,0,100,100,0.00,0.00,1,1,0,2,20,20,20,0`;
+
     let eventsString = `[Events]\nFormat: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text\n`;
+
     // 保存lineCount大小的数组
     // 每个元素保存最后一次出现在行数下标+1的dsArray元素
     // lineRecords[type][dsaArray[num]] type:0 滚动 1 顶部 2底部
@@ -52,9 +60,9 @@ Style: Danmaku,${this.config.font},${this.config.font_size},&H${this.config.alph
       let type = 1;
       const start = ~~danmukuPosition[0];
       let end = start + this.config.speed;
-      let move1 = this.config.PlayResX + (text.length * this.config.font_size) / 2;
-      let move24 = this.config.font_size;
-      const move3 = 0 - (text.length * this.config.font_size) / 2;
+      let move1 = this.config.PlayResX + (text.length * this.config.fontSize) / 2;
+      let move24 = this.config.fontSize;
+      const move3 = 0 - (text.length * this.config.fontSize) / 2;
       const color = prefixInteger((~~danmukuPosition[3]).toString(16), 6).replace(/(.{2})(.{2})(.{2})/, '$3$2$1');
 
       // 获取字幕插入的行
@@ -76,11 +84,11 @@ Style: Danmaku,${this.config.font},${this.config.font_size},&H${this.config.alph
         end = start + this.config.fixedSpeed;
         // 底部弹幕处理
         if (danmukuPosition[1] === 4) {
-          move24 = this.config.PlayResY - (line - 1) * this.config.font_size;
+          move24 = this.config.PlayResY - (line - 1) * this.config.fontSize;
         }
         // 顶部弹幕处理
         if (danmukuPosition[1] === 5) {
-          move24 = line * this.config.font_size;
+          move24 = line * this.config.fontSize;
         }
       }
       // 高级弹幕，告辞
@@ -114,15 +122,18 @@ Style: Danmaku,${this.config.font},${this.config.font_size},&H${this.config.alph
           return i;
         }
         let pStart = parseFloat(lineRecords[0][i].danmukuPosition[0]);
-        let danmakuLength = lineRecords[0][i].danmukuText.length * this.config.font_size;
+        // 使用canvas计算弹幕，缺点：速度慢
+        let danmakuWidth = canvasContext.measureText(lineRecords[0][i].danmukuText).width;
+        // let danmakuWidth = lineRecords[0][i].danmukuText.length * this.config.fontSize;
+        // console.log(lineRecords[0][i].danmukuText, danmakuWidth)
 
         // 待比较弹幕首次完全显示在屏幕的时间
-        let time1 = pStart + (this.config.speed * danmakuLength) / (this.config.PlayResX + danmakuLength);
+        let time1 = pStart + (this.config.speed * danmakuWidth) / (this.config.PlayResX + danmakuWidth);
         // 待比较弹幕完全消失在屏幕的时间
         let time2 = pStart + this.config.speed;
         // 当前弹幕最后一刻完全显示在屏幕的时间
-        danmakuLength = danmukuText.length * this.config.font_size;
-        let time3 = start + (this.config.speed * this.config.PlayResX) / (this.config.PlayResX + danmakuLength);
+        danmakuWidth = danmukuText.length * this.config.fontSize;
+        let time3 = start + (this.config.speed * this.config.PlayResX) / (this.config.PlayResX + danmakuWidth);
         if (start >= time1 && time3 >= time2) {
           // 覆盖原来的
           lineRecords[0][i] = { danmukuPosition, danmukuText };
@@ -147,7 +158,7 @@ Style: Danmaku,${this.config.font},${this.config.font_size},&H${this.config.alph
         }
       }
     }
-    // 返回无穷大
+    // 没位置了返回无穷大
     return Infinity;
   }
 }
