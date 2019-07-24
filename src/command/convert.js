a0bcc0a12574c0fc673f1136fe39514bd083a35f
Myon <myon.cn@gmail.com>
2019-07-24 10:00:20 +0800

    convert添加文件夹处理

--- src/command/convert.js
+++ src/command/convert.js
@@ -1,16 +1,19 @@
 const DanmukuConverter = require('../utils/DanmukuConverter');
 const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 const { promisify } = require('util');
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 const exists = promisify(fs.exists);
 const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
 module.exports = function convert(program) {
   program
     .command('convert')
     .option('-f, --file <string>', '需要转换的xml文件')
     .option('--folder <string>', '需要处理的文件夹（文件和文件夹二选一）')
-    .option('--match', '文件名匹配规则', '.xml$')
+    .option('--match <string>', '文件名匹配规则', '.xml$')
     .option('--play-res-x <number>', '分辨率宽', 1920)
     .option('--play-res-y <number>', '分辨率高', 1080)
     .option('--font <string>', '字体', '微软雅黑')
@@ -26,7 +29,7 @@ module.exports = function convert(program) {
     .option('--alpha <number>', '透明度0-255', 140)
     .action(async args => {
       if (!args.file && !args.folder) {
-        console.error('未指定文件');
+        console.error(chalk.red('未指定文件'));
         return;
       }
       const converter = new DanmukuConverter({
@@ -46,19 +49,38 @@ module.exports = function convert(program) {
           const danmukuStr = converter.convert(xmlContent);
           const filename = args.file.replace('.xml', '.ass');
           await writeFile(filename, danmukuStr);
-          console.log(`output: ${filename}`);
+          console.log(chalk.green(`output: ${filename}`));
         } else {
-          console.error('文件不存在');
+          console.error(chalk.red('文件不存在'));
         }
       } else if (args.folder) {
         if (await exists(args.folder)) {
-          // TODO 文件夹处理
-          const files = await fs.readdir(args.folder);
-          for (let file of files) {
-            console.log(file);
-          }
+          let convertDir = async dir => {
+            const filenames = await readdir(dir);
+            for (let filename of filenames) {
+              const filenameRegex = new RegExp(args.match);
+              const filePath = path.join(dir, filename);
+              const fileStat = await stat(filePath);
+              if (fileStat.isDirectory()) {
+                await convertDir(filePath);
+              } else if (filenameRegex.test(filename)) {
+                if (fileStat.isFile()) {
+                  try {
+                    const xmlContent = await readFile(filePath);
+                    const danmukuStr = converter.convert(xmlContent);
+                    const assFilename = path.join(dir, filename.replace('.xml', '.ass'));
+                    await writeFile(assFilename, danmukuStr);
+                    console.log(chalk.green(`[success] ${filePath} => ${assFilename}`));
+                  } catch (e) {
+                    console.error(chalk.red(`[ error ] ${filePath}`));
+                  }
+                }
+              }
+            }
+          };
+          await convertDir(args.folder);
         } else {
-          console.error('文件夹不存在');
+          console.error(chalk.red('文件夹不存在'));
         }
       }
       process.exit();
